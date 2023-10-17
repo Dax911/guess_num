4f3ab235630ec2d7ebcc01a042e6740e0607e0f4
Dax <haydenaylor911@gmail.com>
2023-10-17 17:46:03 -0500

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
+    let rand_num: i32 = rand::thread_rng().gen_range(1 ..= 101);
 
-    let x = 5;
+    loop{
+        println!("Input your guess.");
+        
+        let mut guess = String::new();
 
-    println!("Guess the number!");
-
-    println!("Input your guess.");
-
-    let mut guess = String::new();
-
-    std::io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    println!("You guessed: {}", guess);
-    //check if the guess is correct
-    if guess.trim().parse::<i32>().unwrap() == x {
-        println!("You guessed correctly!");
-    } else {
-        println!("You guessed incorrectly!");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        println!("You guessed: {}", guess);
+        //check if the guess is correct
+        if guess.trim().parse::<i32>().unwrap() == rand_num {
+            println!("You guessed correctly!");
+            break;
+        }
+        if guess.trim().parse::<i32>().unwrap() > rand_num {
+            println!("Too high!");
+        }
+        if guess.trim().parse::<i32>().unwrap() < rand_num {
+            println!("Too low!");
+        }
     }
+
+    
 }
